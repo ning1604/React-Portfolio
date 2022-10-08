9a80c68d4877c04111e643a951160f183ffab734
Ning <100749559+ning1604@users.noreply.github.com>
2022-10-08 17:40:53 +1100

    added icon links in footer

--- react-portfolio/src/components/Footer.js
+++ react-portfolio/src/components/Footer.js
@@ -3,7 +3,11 @@ import React from 'react';
 function Footer() {
     return (
         <footer>
-            This is a Footer component.
+            <div className='icon-group'>
+                <a href='https://github.com/ning1604' target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github fa-4x"></i></a>
+                <a href='https://www.linkedin.com/in/ning-zhaw-bong-a838ba202/' target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-linkedin fa-4x"></i></a>
+                <a href='https://stackoverflow.com/users/19049450/ning?tab=profile' target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-stack-overflow fa-4x"></i></a>
+            </div>
         </footer>
     )
 }
