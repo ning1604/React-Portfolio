5fa871ac783f20d4d79a12e9c983a9ab2e18f02c
Ning <100749559+ning1604@users.noreply.github.com>
2022-10-09 14:34:17 +1100

    added resume content and styling

--- react-portfolio/src/components/pages/Resume.js
+++ react-portfolio/src/components/pages/Resume.js
@@ -5,9 +5,37 @@ export default function Resume() {
     <div className='container'>
       <div className='page-container'>
         <h1 className='page-title'>Resume Page</h1>
-        <p>
-          Resume sample text
-        </p>
+        <div className='resume-container'>
+          <div className='resume-item'>
+            <h2>
+              Front-end Proficiencies
+            </h2>
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>JQuery</li>
+              <li>Responsive</li>
+              <li>React</li>
+              <li>Bootstrap</li>
+            </ul>
+          </div>
+          <div className='resume-item'>
+            <h2>
+              Back-end Proficiencies
+            </h2>
+            <ul>
+              <li>APIs</li>
+              <li>Node</li>
+              <li>Express</li>
+              <li>MySQL, Sequelize</li>
+              <li>MongoDB, Mongoose</li>
+              <li>REST</li>
+              <li>GraphQL</li>
+            </ul>
+          </div>
+        </div>
+        <a href="#" download="ning_zhaw_resume" className='resume-btn'>download resume</a>
       </div>
     </div>
   );
