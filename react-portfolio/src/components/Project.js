159833b13da5e8c7224a193ac2f484fa2eeb3005
Ning <100749559+ning1604@users.noreply.github.com>
2022-10-11 16:42:41 +1100

    added functionality and styling to portfolio page

--- react-portfolio/src/components/Project.js
+++ react-portfolio/src/components/Project.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
-function Project() {
+function Project(props) {
+    const projects = props.projects;
+
     return (
-        <p>project</p>
-    )
+        <div className='project-list'>
+            {projects.map((project) => (
+                <div className='project-item' key={project.id}>
+                    <div className='card'>
+                    <div className='project-details'>
+                        <h3 className='project-title'>{project.title}</h3>
+                        <a href={project.repoLink} target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-github fa-3x"></i></a>
+                    </div>
+                    <img src={project.image} alt={project.title + 'image'}></img>
+                    <a href={project.deployLink} target="_blank" rel="noopener noreferrer"><div className='deploy-link'></div></a>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default Project;
--- react-portfolio/src/components/pages/Portfolio.js
+++ react-portfolio/src/components/pages/Portfolio.js
@@ -1,12 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Project from '../Project';
 
 export default function Portfolio() {
+const [projects, setProjects] = useState([
+  {
+    id: 1,
+    title: 'Tech Blog',
+    deployLink: 'https://arcane-inlet-77522.herokuapp.com/',
+    repoLink: 'https://github.com/ning1604/Tech-Blog',
+    image: '../assets/images/tech_blog.png',
+  },
+  {
+    id: 2,
+    title: 'Text Editor',
+    deployLink: 'https://secret-anchorage-31431.herokuapp.com/',
+    repoLink: 'https://github.com/ning1604/Text-Editor',
+    image: '../assets/images/text_editor.png',
+  },
+  {
+    id: 3,
+    title: 'Note Taker',
+    deployLink: 'https://aqueous-journey-36664.herokuapp.com/',
+    repoLink: 'https://github.com/ning1604/Note-Taker',
+    image: '../assets/images/note_taker.png',
+  },
+  {
+    id: 4,
+    title: 'Weather Dashboard',
+    deployLink: 'https://ning1604.github.io/Weather-Dashboard/',
+    repoLink: 'https://github.com/ning1604/Weather-Dashboard',
+    image: '../assets/images/weather_dashboard.png',
+  },
+  {
+    id: 5,
+    title: 'Work Day Scheduler',
+    deployLink: 'https://ning1604.github.io/Work-Day-Scheduler/',
+    repoLink: 'https://github.com/ning1604/Work-Day-Scheduler',
+    image: '../assets/images/work_day_scheduler.png',
+  },
+  {
+    id: 6,
+    title: 'Code Quiz',
+    deployLink: 'https://ning1604.github.io/Web-APIs-Code-Quiz/',
+    repoLink: 'https://github.com/ning1604/Web-APIs-Code-Quiz',
+    image: '../assets/images/code_quiz.png',
+  }
+])
+
   return (
     <div className='container'>
       <div className='page-container'>
         <h1 className='page-title'>Portfolio Page</h1>
-        <Project />
+        <div>
+          <Project projects={projects}/>
+        </div>
       </div>
     </div>
   );
