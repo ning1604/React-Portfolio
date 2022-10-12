8e4b4ab5d1276e0c44830962f770dc1d43eebe09
Ning <100749559+ning1604@users.noreply.github.com>
2022-10-12 13:51:41 +1100

    fixed project images not displaying on deployment site

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -8,42 +8,42 @@ const [projects, setProjects] = useState([
     title: 'Tech Blog',
     deployLink: 'https://arcane-inlet-77522.herokuapp.com/',
     repoLink: 'https://github.com/ning1604/Tech-Blog',
-    image: '../assets/images/techBlog.png',
+    image: 'assets/images/techBlog.png',
   },
   {
     id: 2,
     title: 'Text Editor',
     deployLink: 'https://secret-anchorage-31431.herokuapp.com/',
     repoLink: 'https://github.com/ning1604/Text-Editor',
-    image: '../assets/images/textEditor.png',
+    image: 'assets/images/textEditor.png',
   },
   {
     id: 3,
     title: 'Note Taker',
     deployLink: 'https://aqueous-journey-36664.herokuapp.com/',
     repoLink: 'https://github.com/ning1604/Note-Taker',
-    image: '../assets/images/noteTaker.png',
+    image: 'assets/images/noteTaker.png',
   },
   {
     id: 4,
     title: 'Weather Dashboard',
     deployLink: 'https://ning1604.github.io/Weather-Dashboard/',
     repoLink: 'https://github.com/ning1604/Weather-Dashboard',
-    image: '../assets/images/weatherDashboard.png',
+    image: 'assets/images/weatherDashboard.png',
   },
   {
     id: 5,
     title: 'Work Day Scheduler',
     deployLink: 'https://ning1604.github.io/Work-Day-Scheduler/',
     repoLink: 'https://github.com/ning1604/Work-Day-Scheduler',
-    image: '../assets/images/workDayScheduler.png',
+    image: 'assets/images/workDayScheduler.png',
   },
   {
     id: 6,
     title: 'Code Quiz',
     deployLink: 'https://ning1604.github.io/Web-APIs-Code-Quiz/',
     repoLink: 'https://github.com/ning1604/Web-APIs-Code-Quiz',
-    image: '../assets/images/codeQuiz.png',
+    image: 'assets/images/codeQuiz.png',
   }
 ])
 
