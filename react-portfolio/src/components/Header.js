aaf55e5562f1b3af3225058c0f24104c8fbdf3cf
Ning <100749559+ning1604@users.noreply.github.com>
2022-10-12 12:49:04 +1100

    fixed about title highlighted on first load

--- react-portfolio/src/components/Header.js
+++ react-portfolio/src/components/Header.js
@@ -6,7 +6,7 @@ import Contact from './pages/Contact';
 import Portfolio from './pages/Portfolio';
 
 function Header() {
-    const [currentPage, setCurrentPage] = useState('Home');
+    const [currentPage, setCurrentPage] = useState('About');
 
     // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
     const renderPage = () => {
