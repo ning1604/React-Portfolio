2b084b31bcec25e852f8f9993df7c044587ee8a0
Ning <100749559+ning1604@users.noreply.github.com>
2022-10-12 13:01:22 +1100

    updated about me page bio

--- react-portfolio/src/components/pages/About.js
+++ react-portfolio/src/components/pages/About.js
@@ -7,7 +7,7 @@ export default function About() {
         <h1 className='page-title'>About Me</h1>
         <img src="assets/images/myAvatar.png" alt="Avatar" class="avatar"></img>
         <p className='about-content'>
-          Hi! I'm Ning, a passionate full stack web developer.
+          Hi! I'm Ning, a passionate full stack web developer based in Melbourne.
         </p>
       </div>
     </div>
