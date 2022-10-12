676b5b4ab5fe1bce073e8833d5a485055935e979
Ning <100749559+ning1604@users.noreply.github.com>
2022-10-12 12:55:54 +1100

    added avatar to about me page

--- react-portfolio/src/components/pages/About.js
+++ react-portfolio/src/components/pages/About.js
@@ -5,6 +5,7 @@ export default function About() {
     <div className='container'>
       <div className='page-container'>
         <h1 className='page-title'>About Me</h1>
+        <img src="assets/images/myAvatar.png" alt="Avatar" class="avatar"></img>
         <p className='about-content'>
           Hi! I'm Ning, a passionate full stack web developer.
         </p>
